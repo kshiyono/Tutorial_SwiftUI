8d5afbdef08e283a3e66d92c0d8fdac4be77c0e2
kshiyono <k.shiyo.no@gmail.com>
2019-08-13 15:42:54 +0900

    [add] SwiftUIチュートリアル

---
+++ SwiftUiTutorial/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUiTutorial
+//
+//  Created by Katsuyama Shota on 2019/08/13.
+//  Copyright © 2019 Katsuyama Shota. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
